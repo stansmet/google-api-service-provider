769c8eb68356520613c30453c865024092115b61
Stanislaw Smetanin <stanislaw@smetanin-online.name>
2013-07-29 15:39:20 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# Google API Client Service Provider (Silex)
+==================================================================
+
+A Silex Service Provider for Google API Client  
+[https://github.com/stansmet/google-api-php-client](https://github.com/stansmet/google-api-php-client)
+
+## Installation
+--------------
+
+[Composer](http://getcomposer.org) is currently the only way to install the 
+foursquare client into your project.
+
+### Create your composer.json file
+
+      {
+          "require": {
+              "stansmet/google-api-service-provider": "dev-master@dev"
+          }
+      }
+
+### Download composer into your application root
+
+      $ curl -s http://getcomposer.org/installer | php
+
+### Install your dependencies
+
+      $ php composer.phar install
+ 
+## Usage
+---------
+
+Register the service provider
+
+      $app->register(new Stansmet\GoogleApi\Silex\GoogleApiServiceProvider(), array(
+          'google.api.key_file' => __DIR__.'/../google.p12',
+          'google.api.scopes' => ['http://www.google.com/calendar/feeds/'],
+          'google.api.service_account_name' => '474314885299@developer.gserviceaccount.com',
+      ));
+
+
+Get an instance of the [GoogleApi\Client](https://github.com/stansmet/google-api-php-client/blob/master/src/GoogleApi/Client.php
+
+      $client = $app['google.api.client'];
+
+## Links
+--------
+https://code.google.com/p/google-api-php-client/wiki/OAuth2#Service_Accounts
---
+++ lib/Stansmet/GoogleApi/Silex/GoogleApiServiceProvider.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Stansmet\GoogleApi\Silex;
+
+use Silex\Application;
+use Silex\ServiceProviderInterface;
+use GoogleApi\Client;
+use GoogleApi\Auth\AssertionCredentials
+
+class GoogleApiServiceProvider implements ServiceProviderInterface
+{
+    public function register(Application $app)
+    {
+        $app['google.api.client'] = $app->share(function() use ($app) {
+            if (isset($app['google.api.key_file'])) {
+                $key = file_get_contents($app['google.api.key_file']);
+            } else {
+                throw new Exception("Key file must be defined", 1);            
+            }
+            
+            if (isset($app['google.api.service_account_name'])) {
+                $accountName = $app['google.api.service_account_name'];
+            } else {
+                throw new Exception("Service account name must be defined", 1);            
+            }
+
+            if (isset($app['google.api.scopes']) 
+                && is_array($app['google.api.scopes'])) {
+
+                $scopes = $app['google.api.scopes'];
+            } else {
+                throw new Exception("Api scopes must be defined", 1);            
+            }
+
+            $credentials = new AssertionCredentials($accountName, $scopes, $key);
+            $client = new Client;
+            $client->setAssertionCredentials($credentials);
+            $client->setUseObjects(true);
+
+            return $client;
+        });
+    }
+
+    public function boot(Silex\Application $app)
+    {}
+}
